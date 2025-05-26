f66867dfea0e426398179b883f8626a9f365d583
Raphael <raphael.g@grpgformation.onmicrosoft.com>
2025-05-26 11:06:34 +0200

    Première modif

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Utilisateurs() {
   return (
     <div className="bg-blue-300 m-4 p-4 rounded-2xl">
       <h1 className="text-2xl">Projet Next.JS !</h1>
-      <p>Super ça marche !</p>
+      <p>Super ça marche ! En plus je modifie.</p>
       <a href="/utilisateurs">
         <button className="bg-blue-400 m-2 p-2 rounded-2xl border border-amber-100 border-2">
           Liste des utilisateurs
