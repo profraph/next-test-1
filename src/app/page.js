1c55277615decf829765c437721f82f78df5090e
Raphael <raphael.g@grpgformation.onmicrosoft.com>
2025-05-26 10:03:17 +0200

    Résoudre les erreurs de build

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Utilisateurs() {
   return (
     <div className="bg-blue-300 m-4 p-4 rounded-2xl">
-      <h1 className="text-2xl">C'est un projet Next.JS</h1>
+      <h1 className="text-2xl">Projet Next.JS !</h1>
       <p>Super ça marche !</p>
       <a href="/utilisateurs">
         <button className="bg-blue-400 m-2 p-2 rounded-2xl border border-amber-100 border-2">
